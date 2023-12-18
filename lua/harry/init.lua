fc4ac08b0b42643cc852347087fadb12eee1a6e9
hmartin <hmartin@paracomm.co.uk>
2023-12-18 11:21:26 +0000

    initial commit

---
+++ lua/harry/init.lua
@@ -0,0 +1,7 @@
+require("harry.set")
+require("harry.remap")
+require("harry.packer")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ lua/harry/remap.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>h", vim.cmd.ClangdSwitchSourceHeader)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+vim.keymap.set("i", "fd", "<Esc>")
+
+vim.keymap.set("n", "<leader>tn", "<cmd>:tabnew<cr>")
+vim.keymap.set("n", "<leader>th", "<cmd>:tabNext<cr>")
+vim.keymap.set("n", "<leader>tl", "<cmd>:tabnext<cr>")
+vim.keymap.set("n", "<leader>tq", "<cmd>:tabclose<cr>")
+
+vim.keymap.set("n", "<leader>sv", "<cmd>:vsplit<cr>")
+vim.keymap.set("n", "<leader>sh", "<cmd>:split<cr>")
+
+
+vim.cmd('colorscheme base4tone_classic_w_dark')
